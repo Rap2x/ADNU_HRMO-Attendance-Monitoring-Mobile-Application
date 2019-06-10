18e8c74362290a295021f3538890083ece7c2120
Ralph S. Eufracio <reufracio@gbox.adnu.edu.ph>
2019-06-10 10:52:28 +0800

    Adds confirmation slip activity

--- app/src/main/java/com/icst241/adnu_hrmoattendancemonitoringmobileapplication/ConfirmationSlip.java
+++ app/src/main/java/com/icst241/adnu_hrmoattendancemonitoringmobileapplication/ConfirmationSlip.java
@@ -8,12 +8,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class activity_attendance extends AppCompatActivity {
+public class ConfirmationSlip extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attendance);
+        setContentView(R.layout.activity_confirmation_slip);
+
         android.support.v7.widget.Toolbar toolbar =findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
     }
@@ -27,14 +28,5 @@ public class activity_attendance extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
-    }
-    public void markAbsent(View view){
-        Intent intent = new Intent(this, activity_absent.class);
-        startActivity(intent);
-    }
-
-    public void openConfirmationSlip(View view){
-        Intent intent = new Intent(this, ConfirmationSlip.class);
-        startActivity(intent);
     }
 }
